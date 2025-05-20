14e0d69e0acf2ad4296137feed409e91ffdfa861
WajihAlsibai <wajih.sibai2001@gmail.com>
2025-05-20 23:56:36 +0200

    Oppgave 1.0: Initialisert ASP.NET Core MVC prosjekt

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Eksamen2025Gruppe5.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
