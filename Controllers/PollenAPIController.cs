8b54606bf6ddf0e346fc4cec6858496cfd2e2423
WajihAlsibai <wajih.sibai2001@gmail.com>
2025-05-21 14:15:31 +0200

    Oppgave 2.1: Opprettet branch extraFeature, samt PollenAPIController og tilhørende view for visning av data fra eksternt API. Ingen CRUD, kun tom visning foreløpig.

---
+++ Controllers/PollenAPIController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eksamen2025Gruppe5.Controllers
+{
+    public class PollenAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
