8ff7d1039f15862610e34afc149625dfe6bca931
WajihAlsibai <wajih.sibai2001@gmail.com>
2025-05-21 22:14:00 +0200

    Oppdatert PollenAPIViewModel (småjusteringer før merge til main)

--- Models/PollenAPIViewModel.cs
+++ Models/PollenAPIViewModel.cs
@@ -2,14 +2,12 @@ namespace Eksamen2025Gruppe5.Models
 {
     public class PollenAPIViewModel
     {
+        public string? Date { get; set; }
         public string? Code { get; set; }
         public string? DisplayName { get; set; }
         public int Value { get; set; }
         public string? Category { get; set; }
         public string? IndexDescription { get; set; }
-        public string? Date { get; set; }
-
-        // Color-info (RGB)
         public int Red { get; set; }
         public int Green { get; set; }
         public int Blue { get; set; }
